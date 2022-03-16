2c89dfa3d4b0765ce1f0674de31be3bac112bca9
Grzegorz Klimek <klimus50@gmail.com>
2022-03-16 22:33:05 +0100

    create images app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
+import './App.scss';
+
+const IMAGES_PER_PAGE = 3;
 
 function App() {
+  const [slugList, setSlugList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    const response = await fetch('https://picsum.photos/v2/list');
+    const images = await response.json();
+
+    const slugList = images.map(({ url }) => {
+      const urlParts = url.split('/');
+      const slug = urlParts[urlParts.length - 1];
+      return slug;
+    });
+
+    setSlugList(slugList);
+    setIsLoading(false);
+  };
+
+  const handleClick = () => {
+    setPage((page) => {
+      if ((page + 1) * IMAGES_PER_PAGE + IMAGES_PER_PAGE > slugList.length) {
+        return 0;
+      }
+      return page + 1;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="images-container">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          slugList
+            .slice(page * IMAGES_PER_PAGE, page * IMAGES_PER_PAGE + 3)
+            .map((slug) => {
+              return (
+                <img
+                  key={slug}
+                  src={`http://source.unsplash.com/${slug}`}
+                  alt={slug}
+                />
+              );
+            })
+        )}
+      </div>
+      {!isLoading && (
+        <button className="next-images-btn" onClick={handleClick}>
+          Next
+        </button>
+      )}
     </div>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,5 @@
+const Loader = () => (
+  <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+)
+
+export default Loader;
